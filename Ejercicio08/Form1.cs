98284a2447bf9e14f4b5fa22089b4b352a860817
Pablo De Madaria Pascual <pablodemadaria@outlook.es>
2022-03-15 12:21:12 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio08/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ejercicio08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+                //BaseLine v 1.0
+
+
+
+        const int KNUMEROS = 6; // Tanto el sorteo como los números del jugador serán 6.
+        List<int> numerosSorteo = new List<int>();
+        List<int> numerosJugador = new List<int>();
+
+        private void realizarSorteo(List<int> numerosSorteo)
+        {
+            int cont, num;
+            Random r = new Random();
+
+            cont = 0;
+            while(cont < KNUMEROS)
+            {
+                num = r.Next(1, 50);    // Obtenemos un número aleatorio entre 1 y 49
+
+                // Si no ha salido ya el número. Si ha salido no aumentamos el contador
+                if (numerosSorteo.Contains(num) == false)    
+                {
+                    numerosSorteo.Add(num);
+                    cont++;
+                }
+            }
+        }
+
+
+        // Devuelve un string con una lista
+        private string mostrarLista(List<int> lista)
+        {
+            string texto;
+
+            texto = "";
+            for (int i = 0; i < lista.Count; i++)
+                texto = texto + lista[i] + ", ";
+
+            texto = texto + "\n";
+
+            return texto;
+        }
+
+        private void bSorteo_Click(object sender, EventArgs e)
+        {
+            numerosSorteo.Clear();
+            realizarSorteo(numerosSorteo);
+
+            MessageBox.Show(mostrarLista(numerosSorteo));
+        }
+
+
+        private List<int> leerNumerosLoteria(List<int>lista ) //La función lee los numeros del jugador verificando que esten en rango y no repetidos
+        {
+            int valor;
+            int cont = 0;
+
+           while (lista.Count<KNUMEROS)
+            {
+                valor = int.Parse(Interaction.InputBox("Introduzca un valor: "));
+                if (valor > 0 && valor < 50)
+                {
+                    if (lista.Contains(valor) == false)
+                    {
+                        lista.Add(valor);
+                        cont++;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El numero esta repetido");
+                    }
+                }
+                else 
+                {
+                    MessageBox.Show("El numero debe estar comprendido entre 1 y 49");
+                }
+            } 
+
+            return lista;
+        }
+
+
+        string comprobarNumerosSorteo(List<int> lista1, List<int> lista2) { // La funcion recorre una de las listas y en caso de contener algun numero en la otra lista, suma 1 el contador de aciertos y guarda el numero en otra variable.
+            string TextoAciertos = "";
+            int contadorAciertos = 0;
+            string Texto="";
+            foreach (int num in lista1) {
+                if (lista2.Contains(num)) {
+                    contadorAciertos++;
+                    TextoAciertos = TextoAciertos + " ," + num;
+                }            
+             }
+            if (contadorAciertos == 0) //En caso de no haber aciertos, la función devuelve el texto que no ha habido aciertos
+            {
+                Texto = "No hay ningun acierto";
+            }
+            else { //En caso que existan aciertos, la funcion devuleve texto con el numero de aciertos y los numeros acertados
+                Texto = "Han habido " + contadorAciertos+" aciertos" + ". Los numeros acertados son:" + TextoAciertos;
+            }
+
+            return Texto;
+        }
+
+
+        private void bJugar_Click(object sender, EventArgs e)
+        {
+            leerNumerosLoteria(numerosJugador);
+        }
+
+        private void bResultado_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(comprobarNumerosSorteo(numerosSorteo, numerosJugador));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ejercicio08/Form1.designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Ejercicio08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSorteo = new System.Windows.Forms.Button();
+            this.bJugar = new System.Windows.Forms.Button();
+            this.bResultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bSorteo
+            // 
+            this.bSorteo.Location = new System.Drawing.Point(101, 46);
+            this.bSorteo.Name = "bSorteo";
+            this.bSorteo.Size = new System.Drawing.Size(178, 40);
+            this.bSorteo.TabIndex = 0;
+            this.bSorteo.Text = "Realizar Sorteo";
+            this.bSorteo.UseVisualStyleBackColor = true;
+            this.bSorteo.Click += new System.EventHandler(this.bSorteo_Click);
+            // 
+            // bJugar
+            // 
+            this.bJugar.Location = new System.Drawing.Point(101, 132);
+            this.bJugar.Name = "bJugar";
+            this.bJugar.Size = new System.Drawing.Size(177, 43);
+            this.bJugar.TabIndex = 1;
+            this.bJugar.Text = "Introducir números a jugar";
+            this.bJugar.UseVisualStyleBackColor = true;
+            this.bJugar.Click += new System.EventHandler(this.bJugar_Click);
+            // 
+            // bResultado
+            // 
+            this.bResultado.Location = new System.Drawing.Point(101, 215);
+            this.bResultado.Name = "bResultado";
+            this.bResultado.Size = new System.Drawing.Size(176, 46);
+            this.bResultado.TabIndex = 2;
+            this.bResultado.Text = "Resultado";
+            this.bResultado.UseVisualStyleBackColor = true;
+            this.bResultado.Click += new System.EventHandler(this.bResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 298);
+            this.Controls.Add(this.bResultado);
+            this.Controls.Add(this.bJugar);
+            this.Controls.Add(this.bSorteo);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 8. Pablo de Madaria Pascual. DAW-Z";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSorteo;
+        private System.Windows.Forms.Button bJugar;
+        private System.Windows.Forms.Button bResultado;
+    }
+}
+
